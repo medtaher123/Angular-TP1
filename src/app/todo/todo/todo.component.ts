9eec450810573fefd122dbb6e05bc32fe07721a3
medtaher <medtaherbh@gmail.com>
2025-10-16 22:30:22 +0100

    feat: arc-en-ciel directive in standalone app

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -3,6 +3,7 @@ import { Todo } from '../model/todo';
 import { TodoService } from '../service/todo.service';
 
 import { FormsModule } from '@angular/forms';
+import { ArcEnCielDirective } from 'src/app/arc-en-ciel.directive';
 
 @Component({
     selector: 'app-todo',
@@ -10,7 +11,7 @@ import { FormsModule } from '@angular/forms';
     styleUrls: ['./todo.component.css'],
     providers: [TodoService],
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule,ArcEnCielDirective],
 })
 export class TodoComponent {
   private todoService = inject(TodoService);
