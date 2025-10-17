1f4463596d81a8eb7d0700273ce8dda601f86ef9
medtaher <medtaherbh@gmail.com>
2025-10-17 09:06:06 +0100

    feat: create ttc standalone component

---
+++ src/app/ttc/ttc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ttc',
+  standalone: true,
+  imports: [],
+  templateUrl: './ttc.component.html',
+  styleUrl: './ttc.component.css'
+})
+export class TTCComponent {
+
+}
