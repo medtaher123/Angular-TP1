3e5a2d0d065eb0107efc48357ce03917f69d07d2
medtaher <medtaherbh@gmail.com>
2025-10-18 16:04:27 +0100

    feat: ttc component

--- src/app/ttc/ttc.component.ts
+++ src/app/ttc/ttc.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-ttc',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ttc.component.html',
   styleUrl: './ttc.component.css'
 })
 export class TTCComponent {
+
+  prix: WritableSignal<number> = signal(0)
+  quantite : WritableSignal<number> = signal(1)
+  tva: WritableSignal<number> = signal(18)
+  prix_unitaire_ttc = computed(() => this.prix() * (1+this.tva()/100))
+  prix_total_ttc = computed(() => this.quantite() * this.prix() * (1+this.tva()/100))
+  discount = computed(()=> 0)
+
 
 }
