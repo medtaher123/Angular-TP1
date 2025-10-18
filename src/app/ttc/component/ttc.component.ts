fd9714b052ca660efb4dcbbc3bcb43a0ad0686ea
medtaher <medtaherbh@gmail.com>
2025-10-18 16:09:05 +0100

    feat: calculate discount

--- src/app/ttc/component/ttc.component.ts
+++ src/app/ttc/component/ttc.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TTCComponent {
 
-  quantite = 1;  
+  quantite = 1;
   prix = 0;
   tva = 18;
   prix_unitaire_ttc = 0;
@@ -17,6 +17,7 @@ export class TTCComponent {
   calculate(){
     this.prix_unitaire_ttc = this.prix * (1+this.tva/100)
     this.prix_total_ttc = (this.quantite * this.prix) * (1+this.tva/100)
+    this.discount = this.prix_total_ttc * (this.quantite < 10 ? 0 : this.quantite <= 15 ? 0.2 : 0.3)
   }
   
 
