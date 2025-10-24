c5d56a4315768702879f464eb5385c3e538911e5
Yassine Sallemi <yassine.sallemi.251@gmail.com>
2025-10-24 23:14:44 +0100

    feat: added error method

--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -8,4 +8,11 @@ export class LoggerService {
     console.log('From Logger Service :');
     console.log(something);
   }
+
+  error(message: string, error?: any) {
+    console.error('From Logger Service - ERROR: ' + message);
+    if (error) {
+      console.error(error);
+    }
+  }
 }
