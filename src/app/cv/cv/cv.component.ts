81a155d4dbc6452d3af343e5939641ad07f5c73e
Yassine Sallemi <yassine.sallemi.251@gmail.com>
2025-10-24 23:13:56 +0100

    refactor: implement reactive patterns

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -3,15 +3,15 @@ import { Cv } from "../model/cv";
 import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
+import { Observable, catchError, of } from "rxjs";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
   styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
-  /*   selectedCv: Cv | null = null; */
+  cvs$: Observable<Cv[]>;
+  selectedCv$: Observable<Cv | null>;
   date = new Date();
 
   constructor(
@@ -19,19 +19,17 @@ export class CvComponent {
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
+    this.cvs$ = this.cvService.getCvs().pipe(
+      catchError((error) => {
+        this.logger.error('Erreur lors du chargement des CVs:', error);
         this.toastr.error(`
           Attention!! Les données sont fictives, problème avec le serveur.
           Veuillez contacter l'admin.`);
-      },
-    });
+        return of(this.cvService.getFakeCvs());
+      })
+    );
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+    this.selectedCv$ = this.cvService.selectCv$;
   }
 }
