628c18788a99170f83e2aedcdd5c6bfe51da2c22
Jouini-Nour <nour.jouini.558@gmail.com>
2025-11-20 12:33:03 +0100

    finished produts interface and fetching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,14 @@ import { CvComponent } from "./cv/cv/cv.component";
 import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
 import { RhComponent } from "./optimizationPattern/rh/rh.component";
 import { TTCComponent } from "./ttc/component/ttc.component";
+import { ProductsComponent } from "./products/products.component";
 
 const routes: Route[] = [
   { path: "login", component: LoginComponent },
   { path: "rh", component: RhComponent },
+  {
+    path: "products", component: ProductsComponent
+  },
   {
     path: "cv",
     component: CvComponent,
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,10 +6,19 @@ import {
   map,
   takeWhile,
   scan,
+  startWith,
 } from "rxjs";
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
 import { Settings } from "./dto/product-settings.dto";
+
+interface PageInfo{
+  products: Product[],
+  total: number,
+  skip: number,
+  hasMore: boolean,
+
+}
 
 @Component({
   selector: "app-products",
@@ -19,6 +28,42 @@ import { Settings } from "./dto/product-settings.dto";
 export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
+  private loadMore$ = new BehaviorSubject<void>(undefined);
+
+
+  products$!: Observable<PageInfo>;
+
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.loadMore$.pipe(
+
+      concatMap((_,index)=>{
+        const skip = index *12;
+        const limit = 12;
+        return this.productService.getProducts({limit,skip}).pipe(
+          map((response) =>({
+            products: response.products,
+            total: response.total,
+            skip: response.skip + response.products.length,
+            hasMore: response.skip +response.products.length < response.total,
+          }))
+        );
+      }),
+      takeWhile((page) => page.hasMore ||page.products.length ===0, true),
+
+
+      scan((acc: PageInfo, current: PageInfo)=> ({
+        products: [...acc.products, ...current.products],
+        total: current.total,
+        skip: current.skip,
+        hasMore: current.hasMore,
+      }), {products: [], total: 0, skip: 0, hasMore: true} as PageInfo),
+    );
+
+  }
+  onLoadMore() : void {
+    this.loadMore$.next();
+  }
 }
