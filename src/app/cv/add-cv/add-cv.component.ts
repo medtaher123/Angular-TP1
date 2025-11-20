6eda9c3b7bd839f92e8cce7eef3c223db9256a9a
Jouini-Nour <nour.jouini.558@gmail.com>
2025-11-20 13:47:24 +0100

    Ajout des contraintes sur path pour les mineurs

--- src/app/cv/add-cv/add-cv.component.ts
+++ src/app/cv/add-cv/add-cv.component.ts
@@ -1,14 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import {
   AbstractControl,
   FormBuilder,
   Validators,
+  FormsModule,
+  ReactiveFormsModule
 } from "@angular/forms";
 import { CvService } from "../services/cv.service";
 import { Router } from "@angular/router";
 import { ToastrService } from "ngx-toastr";
 import { APP_ROUTES } from "src/config/routes.config";
 import { Cv } from "../model/cv";
+import { JsonPipe } from "@angular/common";
+import {startWith, tap} from "rxjs";
 
 @Component({
   selector: "app-add-cv",
@@ -16,12 +20,10 @@ import { Cv } from "../model/cv";
   styleUrls: ["./add-cv.component.css"],
 })
 export class AddCvComponent {
-  constructor(
-    private cvService: CvService,
-    private router: Router,
-    private toastr: ToastrService,
-    private formBuilder: FormBuilder
-  ) {}
+  private cvService = inject(CvService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+  private formBuilder = inject(FormBuilder);
 
   form = this.formBuilder.group(
     {
@@ -44,11 +46,27 @@ export class AddCvComponent {
     },
   );
 
+  ngOnInit(): void {
+    this.form.get('age')!.valueChanges.pipe(
+      startWith(this.form.get('age')!.value),
+      tap((age:number | null) =>{
+        const pathControl = this.form.get('path')!;
+
+        if ((age!== null && age <18) || (age === null)){
+          pathControl.disable({emitEvent: false});
+          pathControl.setValue('', {emitEvent: false});
+        } else{
+          pathControl.enable({emitEvent: false});
+        }
+      })
+    ).subscribe();
+  }
+
   addCv() {
-    this.cvService.addCv(this.form.value as Cv).subscribe({
+    this.cvService.addCv(this.form.getRawValue() as Cv).subscribe({
       next: (cv) => {
         this.router.navigate([APP_ROUTES.cv]);
-        this.toastr.success(`Le cv ${cv.firstname} ${cv.name}`);
+        this.toastr.success(`Le cv ${cv.firstname} ${cv.name} a été ajouté`);
       },
       error: (err) => {
         this.toastr.error(
